8ce50f9a8ae1952f5bd476e902398ab87c33170c
Pablo-Miguel <pablomigueldelcastillo@gmail.com>
2022-10-27 11:15:05 +0200

    V5

--- app/src/main/java/com/example/acdat_pizzeria/MainActivity.java
+++ app/src/main/java/com/example/acdat_pizzeria/MainActivity.java
@@ -36,7 +36,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 startActivity(intentRegistro);
                 break;
             case R.id.btnIniciarSesion:
-                Usuario usuario = new Usuario(binding.edTextUsuario.getText().toString());
+                Usuario usuario = new Usuario(binding.edTextUsuario.getText().toString().toLowerCase());
 
                 if(Servicio.getInstance().getUsuarios().contains(usuario)){
                     Boolean iniciar = Servicio.getInstance().getUsuario(usuario).getPassword().equals(binding.edTextPassword.getText().toString());
@@ -48,11 +48,30 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
                     }
                     else{
+                        AlertDialog.Builder dialogo1 = crearDialogo("Error al iniciar sesión", "El usuario o la constraseña es incorrecta");
 
+                        dialogo1.setPositiveButton("Aceptar", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+
+                            }
+                        });
+
+                        dialogo1.show();
                     }
 
                 } else {
+                    AlertDialog.Builder dialogo1 = crearDialogo("El usuario no existe", "Registrese para poder acceder");
 
+                    dialogo1.setPositiveButton("Aceptar", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            Intent intentRegistro = new Intent(MainActivity.this, Registrar.class);
+                            startActivity(intentRegistro);
+                        }
+                    });
+
+                    dialogo1.show();
                 }
 
                 break;
@@ -62,10 +81,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onBackPressed() {
-        AlertDialog.Builder dialogo1 = new AlertDialog.Builder(this);
-        dialogo1.setTitle("Salir de la aplicación");
-        dialogo1.setMessage("¿Desea salir de la aplicación?");
-        dialogo1.setCancelable(false);
+        AlertDialog.Builder dialogo1 = crearDialogo("Salir de la aplicación", "¿Desea salir de la aplicación?");
 
         dialogo1.setPositiveButton("Sí", new DialogInterface.OnClickListener() {
             @Override
@@ -82,4 +98,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         dialogo1.show();
     }
+
+    private AlertDialog.Builder crearDialogo(String titulo, String mensaje){
+        AlertDialog.Builder dialogo1 = new AlertDialog.Builder(this);
+        dialogo1.setTitle(titulo);
+        dialogo1.setMessage(mensaje);
+        dialogo1.setCancelable(false);
+
+        return dialogo1;
+    }
+
 }
